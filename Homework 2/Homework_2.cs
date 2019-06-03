dc1c9911632f9985643db3039ff2debed26fa4c2
Golgovsky <kristalizator@gmail.com>
2019-06-03 23:03:48 +0300

    Update (checks)

--- Homework 2/Homework_2.cs
+++ Homework 2/Homework_2.cs
@@ -10,35 +10,66 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            int a, b, h;
-            Console.WriteLine("Please enter A:");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter B:");
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter H:");
-            h = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Result:");
-            if (a < b)
-              {
-                  for (int i = a; i <= b; i += h)
-                  {
+            double a, b, h;
+            int counter = 0;
+            while (true)
+                {
+                while (true) //check
+                {
                     try
                     {
-                        double x = Math.Log((Math.Sqrt(2 - i) + 1.2) / (2 + i) + Math.Pow((2 / i), (1 / 3)));
-                        Console.WriteLine(i + ". " + "X =" + x);
+                        Console.WriteLine("Please enter A:");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Please enter B:");
+                        b = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Please enter H:");
+                        h = Convert.ToDouble(Console.ReadLine());
+                        break;
                     }
-                    catch
+                    catch     //so warning
                     {
-                        Console.WriteLine("Impossible to compute");
+                        Console.WriteLine("Wrong data type!");
                     }
-                  }
-                  Console.ReadKey();
-              }
-              else
-              {
-                  Console.WriteLine("B < A");
-              }
-              
+                }
+                Console.WriteLine("Result:");
+                // time start
+                var sw = System.Diagnostics.Stopwatch.StartNew();
+                //math
+                if (a < b && h > 0)              //check A < B
+                {
+                    for (double i = a; i <= b; i += h)
+                    {
+                        counter++;
+                        if ((i <= 2) && (i != -2) && (i != 0))  //check
+                        {
+                            double x = Math.Sqrt(2 - i) + 1.2;
+                            x = x / (2 + i);
+                            x = x + Math.Pow((2 / i), (1 / 3));
+                            if (x > 0)
+                            {
+                                x = Math.Log(x);
+                                Console.WriteLine(counter + ". " + "X = " + i + ", " + "Y = " + x);
+                            }
+                            else
+                            {
+                                Console.WriteLine(counter + ". " + "X = " + i + ", " + "Y = Error");
+                            }
+
+                        }
+                        else        //so warning
+                        {
+                            Console.WriteLine(counter + ". " + "X = " + i + ", " + "Y = Error");
+                        }
+                    }
+                }
+                else                    //so warning
+                {
+                    Console.WriteLine("Wrong input");
+                }
+                sw.Stop();
+                Console.WriteLine("Time taken: {0}ms", sw.Elapsed.TotalMilliseconds); //time output
+                Console.WriteLine("-------------");
+            }
         }
     }
 }
